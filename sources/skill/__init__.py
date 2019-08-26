9ef6aef64537fa838e41e0ba4b6a2cf24bbf098b
pbillerot <philippe.billerot@gmail.com>
2019-08-26 16:57:28 +0200

    sans hello

--- sources/skill/__init__.py
+++ sources/skill/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask_ask_sdk.skill_adapter import SkillAdapter
 
 from .skill import sb
-from .hello import main
+#from .hello import main
 
 def create_app():
   print('Load Skill..')
   app = Flask(__name__)
-  app.register_blueprint(main)
+  #app.register_blueprint(main)
   skill_adapter = SkillAdapter(
     skill=sb.create(), 
     skill_id="amzn1.ask.skill.bd7515ac-93e7-48c6-b2b5-58dcd0fb0951", 
